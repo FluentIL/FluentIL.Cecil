a3464e50b3cd69629eb255f5204eab6b6ab19337
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2015-10-22 13:53:30 -0200

    moving tests from fluentil project

---
+++ test/FluentIL.Cecil.Tests/Basic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace FluentIL.Cecil.Tests
+{
+    [TestFixture]
+    public class CecilTests
+    {
+        [Test]
+        public void CanAddNewMethodToType()
+        {
+            var assemblyName = new AssemblyNameDefinition("TempAssembly", new Version(1, 0));
+            var assembly = AssemblyDefinition.CreateAssembly(assemblyName, "TempModule", ModuleKind.Dll);
+
+            var typeDefinition = new TypeDefinition("TempNamespace", "TempType", TypeAttributes.Public);
+
+            typeDefinition
+                .NewMethod("TempMethod", MethodAttributes.Public, typeof(string), assembly)
+                .Nop()
+                .Ldstr("Ola")
+                .Ret();
+
+            var methodDefinition = typeDefinition.Methods.First(x => x.Name == "TempMethod");
+            methodDefinition.Body.Instructions.Count.Should().Be.EqualTo(3);
+        }
+    }
+}
